f92872a85b4144240a53d14f674f1b6b733fe5cd
Mikael Biström <info@studiotriangular.se>
2017-12-07 21:47:36 +0100

    Adding path helper

--- app/Lottery.php
+++ app/Lottery.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Lottery extends Model
 {
     protected $fillable = ['name'];
+
+    public function path($action = 'show')
+    {
+        return route("lottery.$action", $this);
+    }
 }
